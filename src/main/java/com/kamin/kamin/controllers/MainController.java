22b99c6ff136799c0fad6f11490e920b194281c6
Artem K <artymk77@gmail.com>
2020-04-15 13:27:25 +0300

    add authorName in DB

--- src/main/java/com/kamin/kamin/controllers/MainController.java
+++ src/main/java/com/kamin/kamin/controllers/MainController.java
@@ -1,14 +1,16 @@
 package com.kamin.kamin.controllers;
 
 import com.kamin.kamin.domain.Message;
+import com.kamin.kamin.domain.User;
 import com.kamin.kamin.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+
 import java.util.Map;
 
 import static com.sun.org.apache.xml.internal.serializer.utils.Utils.messages;
@@ -33,8 +35,12 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model
+    ){
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
--- src/main/java/com/kamin/kamin/domain/Message.java
+++ src/main/java/com/kamin/kamin/domain/Message.java
@@ -1,27 +1,33 @@
 package com.kamin.kamin.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    private String text;
+    private String tag;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
 
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
+        this.author = user;
         this.text = text;
         this.tag = tag;
+
     }
 
-    private String text;
+    public String getAuthorName(){
+        return author!= null ? author.getUsername():"<none>";
+    }
 
-    private String tag;
 
     public void setText(String text) {
         this.text = text;
@@ -29,6 +35,14 @@ public class Message {
 
     public Integer getId() {
         return id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 
     public void setId(Integer id) {
